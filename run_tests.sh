34ee112262c4f182c6573098f352f7bc12e24202
Ilya Beda <ir4y.ix@gmail.com>
2017-10-25 14:57:53 +0300

    Setup for piter-py

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 docker-compose up -d
-docker-compose -f docker-compose.yml -f docker-compose.test.yml run --rm wait_for
 docker-compose -f docker-compose.yml -f docker-compose.test.yml run --rm tests $@
